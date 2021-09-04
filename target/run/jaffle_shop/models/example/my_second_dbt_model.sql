8bf7282842e965db4362ab446c890b0188d5bd7f
Koby Kilimnik <koby@cynerio.co>
2021-09-04 11:45:12 +0300

    dbt initial commit with customers table

---
+++ target/run/jaffle_shop/models/example/my_second_dbt_model.sql
@@ -0,0 +1,10 @@
+
+
+  create or replace view `dbttesting-324906`.`dbt_jake`.`my_second_dbt_model`
+  OPTIONS()
+  as -- Use the `ref` function to select from other models
+
+select *
+from `dbttesting-324906`.`dbt_jake`.`my_first_dbt_model`
+where id = 1;
+
